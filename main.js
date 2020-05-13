29135946a72a050b6a5ef28fe65e3ba340723270
Jakub Bauza <bulzor95@gmail.com>
2020-05-13 13:09:01 +0200

    stop autoscroll after user interact

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ submit.addEventListener("click", (e) => {
   document.getElementById("titleinput").value = "";
   document.getElementById("artistinput").value = "";
 });
-$("html,body").scroll(function () {
-  $(this).stop(true, false);
-});
+//stop autoscroll after user interact
+$("body,html").bind(
+  "scroll mousedown wheel DOMMouseScroll mousewheel keyup",
+  function (e) {
+    if (e.which > 0 || e.type == "mousedown" || e.type == "mousewheel") {
+      $("html,body").stop();
+    }
+  }
+);
